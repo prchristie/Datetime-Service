17ef6199cea6e3f83591623461bee6ad499fce92
Your Name <you@example.com>
2022-03-14 12:37:09 +1030

    Added a service that will convert days to a specified format and convert a datetime to utc when given a timezone

---
+++ Model/Services/DateTimeUtilService.cs
@@ -0,0 +1,30 @@
+﻿using Model.Models;
+
+namespace Model.Services
+{
+    public class DateTimeUtilService : IDateTimeUtilService
+    {
+        public float Convert(float days, TimeFormat format)
+        {
+            switch (format)
+            {
+                case TimeFormat.SECONDS:
+                    return days * 24 * 60 * 60;
+                case TimeFormat.MINUTES:
+                    return days * 24 * 60;
+                case TimeFormat.HOURS:
+                    return days * 24;
+                case TimeFormat.YEARS:
+                    return days / 365f;
+                default:
+                    return days;
+            }
+        }
+
+        public DateTime ToUtc(DateTime date, string timezoneId)
+        {
+            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+            return TimeZoneInfo.ConvertTimeToUtc(date, tz);
+        }
+    }
+}
---
+++ Model/Services/IDateTimeUtilService.cs
@@ -0,0 +1,11 @@
+﻿using Model.Models;
+
+namespace Model.Services
+{
+
+    public interface IDateTimeUtilService
+    {
+        public float Convert(float days, TimeFormat format);
+        public DateTime ToUtc(DateTime date, string timezoneId);
+    }
+}
