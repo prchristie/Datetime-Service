11b9e7e9ddc29a1c7f4ce82a76f1b3048f61603a
Your Name <you@example.com>
2022-03-14 11:49:42 +1030

    Added a models library for storing base classes and services that will be used to do actual calculations

---
+++ Model/Models/DatetimeRange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public class DatetimeRange
+    {
+        public DatetimeRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTime Start { get; }
+        public DateTime End { get; }
+    }
+}
---
+++ Model/Models/TimeFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public enum TimeFormat
+    {
+        SECONDS = 0,
+        MINUTES = 1,
+        HOURS = 2,
+        DAYS = 3,
+        YEARS = 4,
+    }
+}
